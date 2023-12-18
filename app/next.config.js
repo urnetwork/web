549af5f0b6065ff5f333f1861dd4ab3e99b9df66
Awais Hussain <aw@ishussa.in>
2023-12-18 18:32:15 +0000

    Add NextJS build step to makefile

--- app/next.config.js
+++ app/next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    distDir: 'build',
+<<<<<<< Updated upstream
+    output: 'export'
+=======
+    output: 'export',
+>>>>>>> Stashed changes
+}
 
 module.exports = nextConfig
