e73f19ddc2d7fbde75899bbfe1cb9269616a9ec7
Awais Hussain <aw@ishussa.in>
2023-12-19 17:53:52 +0000

    Add template pages for the 4 sections the page will have

--- app/next.config.js
+++ app/next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     distDir: 'build',
-<<<<<<< Updated upstream
-    output: 'export'
-=======
     output: 'export',
->>>>>>> Stashed changes
 }
 
 module.exports = nextConfig
