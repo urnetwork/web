080f610e402f5d6aea7636969774854c6ba88a1a
Awais Hussain <aw@ishussa.in>
2024-01-04 14:25:56 -0500

    Show basic header bar for stats on the provide screen

--- app/src/app/(pages)/provide/page.tsx
+++ app/src/app/(pages)/provide/page.tsx
@@ -4,13 +4,57 @@ import { DevicePhoneMobileIcon } from "@heroicons/react/24/outline";
 import { PlusIcon } from "@heroicons/react/24/solid";
 import { useQuery } from "@tanstack/react-query";
 import Loading from "./loading";
-import { getNetworkClients } from "@lib/api";
+import { getStatsProvidersOverviewLast90 } from "@lib/api";
+import { useState } from "react";
+
+type HeaderItem = {
+  name: string;
+  key: string;
+  pre_unit?: string;
+  unit?: string;
+  helpText?: string;
+};
+
+const headerItems: Array<HeaderItem> = [
+  {
+    name: "Data transferred",
+    key: "transfer_data",
+    unit: "GiB",
+  },
+  {
+    name: "Income",
+    key: "payout",
+    pre_unit: "$",
+  },
+  {
+    name: "Uptime",
+    key: "uptime",
+    unit: "hours",
+  },
+  {
+    name: "Searches",
+    key: "search_interest",
+  },
+  {
+    name: "Devices",
+    key: "clients",
+  },
+];
 
 export default function Page() {
-  const { isPending, data: clients } = useQuery({
-    queryKey: ["network", "clients"],
-    queryFn: async () => (await getNetworkClients()).clients,
+  const [activeStat, setActiveStat] = useState("transfer_data");
+
+  const { isPending, data: stats } = useQuery({
+    queryKey: ["stats", "providers", "overviewLast90"],
+    queryFn: async () => await getStatsProvidersOverviewLast90(),
   });
+
+  const getStatToday = (key: string) => {
+    if (stats && stats[key]) {
+      return stats[key]["2024-01-04"];
+    }
+    return "";
+  };
 
   return (
     <>
@@ -22,40 +66,77 @@ export default function Page() {
         {isPending && <Loading />}
 
         {!isPending && (
-          <>
-            <div className="flex flex-row flex-wrap gap-4">
-              {clients?.length == 0 && (
-                <div className="text-gray-600">No devices found</div>
-              )}
-
-              {clients?.map((client) => (
-                <div
-                  key={client.client_id}
-                  className="border border-gray-300 bg-gray-100 rounded-md w-40 h-96 p-4 flex flex-col gap-4 items-center cursor-pointer hover:border-primary transition-all duration-150 hover:scale-[1.02] text-center"
-                >
-                  <div className="mt-4 flex-flex-col gap-2">
-                    <div className="flex flex-row gap-2 items-center justify-end">
-                      <p className="text-xs text-gray-500">Interest</p>
-                      <div className="w-16 h-3 bg-gray-300 rounded-sm"></div>
-                    </div>
-                    <div className="flex flex-row gap-2 items-center justify-end">
-                      <p className="text-xs text-gray-500">Usage</p>
-                      <div className="w-16 h-3 bg-gray-300 rounded-sm"></div>
-                    </div>
-                  </div>
-
-                  <div className="w-8 h-8 mt-6">
-                    <DevicePhoneMobileIcon className="text-gray-400" />
-                  </div>
-                  <p className="text-sm text-gray-600">{client.description}</p>
-
-                  <p className="text-xs text-gray-400">
-                    This is just a placeholder for now
-                  </p>
-                </div>
-              ))}
+          <div className="flex flex-col gap-y-8">
+            {/* Header showing stats */}
+            <div className="statsBar">
+              <p className="text-sm text-gray-400 font-semibold mb-2">
+                Past 24 hours
+              </p>
+              <div className="flex flex-row w-full gap-2">
+                {headerItems.map((item) => {
+                  return (
+                    <>
+                      <div
+                        onClick={() => {
+                          setActiveStat(item.key);
+                        }}
+                        className={`w-1/${
+                          headerItems.length || 1
+                        } flex flex-col gap-y-2 justify-between items-start border py-2 px-3 rounded-md cursor-pointer hover:border-indigo-600 shadow-md ${
+                          activeStat == item.key
+                            ? "border-indigo-600 border-2"
+                            : "border-gray-300"
+                        }`}
+                      >
+                        <h3
+                          className={`text-sm text-gray-500 ${
+                            activeStat == item.key
+                              ? "font-semibold text-indigo-600"
+                              : ""
+                          }`}
+                        >
+                          {item.name}
+                        </h3>
+                        <p>
+                          <span
+                            className={`text-2xl font-semibold ${
+                              activeStat == item.key
+                                ? "text-indigo-600"
+                                : "text-gray-800"
+                            }`}
+                          >
+                            {item.pre_unit}
+                            {getStatToday(item.key)}
+                          </span>{" "}
+                          <span
+                            className={`text-sm ${
+                              activeStat == item.key
+                                ? "text-indigo-600"
+                                : "text-gray-500"
+                            }`}
+                          >
+                            {item.unit}
+                          </span>
+                        </p>
+                      </div>
+                    </>
+                  );
+                })}
+              </div>
             </div>
-          </>
+
+            {/* 90 Day chart */}
+            <div className="statsChart">
+              <p className="text-sm text-gray-400 font-semibold mb-2">
+                Past 90 days
+              </p>
+              <div className="w-full h-64 bg-gray-100"></div>
+            </div>
+            <div className="devices">
+              <h2 className="mb-2">Devices</h2>
+              <div className="w-full h-64 bg-gray-100"></div>
+            </div>
+          </div>
         )}
       </div>
     </>
--- app/src/app/_lib/types.ts
+++ app/src/app/_lib/types.ts
@@ -1,44 +1,56 @@
 export type AuthCodeLoginResult = {
-    by_jwt: string,
-    error?: {
-        message: string,
-    }
-}
+  by_jwt: string;
+  error?: {
+    message: string;
+  };
+};
 
 export type Client = {
-    client_id: string,
-    description: string,
-    device_spec: string,
-    network_id: string,
-    connections: object[],
-    provide_mode: number
-}
+  client_id: string;
+  description: string;
+  device_spec: string;
+  network_id: string;
+  connections: object[];
+  provide_mode: number;
+};
 
 export type NetworkClientsResult = {
-    clients: Array<Client>,
-}
+  clients: Array<Client>;
+};
 
 type WalletInfo = {
-    wallet_id: string,
-    token_id: string,
-    blockchain: string,
-    blockchain_symbol: string,
-    create_date: string,
-    balance_usdc_nano_cents: 0,
-    address: string,
-}
+  wallet_id: string;
+  token_id: string;
+  blockchain: string;
+  blockchain_symbol: string;
+  create_date: string;
+  balance_usdc_nano_cents: 0;
+  address: string;
+};
 
 type SubscriptionInfo = {
-    subscription_id: string,
-    store: string,
-    plan: string,
-}
+  subscription_id: string;
+  store: string;
+  plan: string;
+};
 
 export type SubscriptionBalanceResult = {
-    balance_byte_count: number,
-    current_subscription: SubscriptionInfo,
-    active_transfer_balances: Array<object>,
-    pending_payout_usd_nano_cents: number,
-    wallet_info: WalletInfo,
-    update_time: string,
-}
+  balance_byte_count: number;
+  current_subscription: SubscriptionInfo;
+  active_transfer_balances: Array<object>;
+  pending_payout_usd_nano_cents: number;
+  wallet_info: WalletInfo;
+  update_time: string;
+};
+
+export type StatsProvidersOverviewLast90Result = {
+  [index: string]: any;
+  lookback: number;
+  created_time: string;
+  uptime: { [key: string]: number };
+  transfer_data: { [key: string]: number };
+  payout: { [key: string]: number };
+  search_interest: { [key: string]: number };
+  contracts: { [key: string]: number };
+  clients: { [key: string]: number };
+};
