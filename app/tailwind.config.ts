574b38a75f844949a485ec335699d4ceac1f729d
Awais Hussain <aw@ishussa.in>
2024-01-09 15:22:34 -0500

    Add tabs to Providers sidebar

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -1,27 +1,27 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        'ok': '#15cd72',
-        'danger': '#CD4439',
-        'primary': {
-          DEFAULT: '#1D3150',
-          'semidark': '#1a2c48', 
-          'dark': '#172740'
+        ok: "#15cd72",
+        danger: "#CD4439",
+        primary: {
+          DEFAULT: "#1D3150",
+          semidark: "#1a2c48",
+          dark: "#172740",
         },
-        'secondary': {
-          DEFAULT: '#A5ACB9'
-        }
-      }
+        secondary: {
+          DEFAULT: "#A5ACB9",
+        },
+      },
     },
   },
-  plugins: [],
-}
-export default config
+  plugins: [require("@headlessui/tailwindcss")],
+};
+export default config;
