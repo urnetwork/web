7c50a86b0e2d358f62e65b18ee99a2704b4c5925
Awais Hussain <aw@ishussa.in>
2024-01-09 14:54:38 -0500

    Charts show in the providers sidebar

--- app/src/app/(pages)/provide/components/DeviceDetailSidebar.tsx
+++ app/src/app/(pages)/provide/components/DeviceDetailSidebar.tsx
@@ -4,6 +4,40 @@ import { Dialog, Popover, Transition } from "@headlessui/react";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 import { useQuery } from "@tanstack/react-query";
 import { Fragment, useState } from "react";
+import { BarChart } from "./Chart";
+
+type ChartItem = {
+  name: string;
+  key: string;
+  pre_unit?: string;
+  unit?: string;
+};
+
+const charts: Array<ChartItem> = [
+  {
+    name: "Data transferred",
+    key: "transfer_data",
+    unit: "GiB",
+  },
+  {
+    name: "Income",
+    key: "payout",
+    pre_unit: "$",
+  },
+  {
+    name: "Uptime",
+    key: "uptime",
+    unit: "hours",
+  },
+  {
+    name: "Searches",
+    key: "search_interest",
+  },
+  {
+    name: "Devices",
+    key: "clients",
+  },
+];
 
 type DeviceDetailSidebarProps = {
   selectedProvider: Provider24h | undefined;
@@ -28,10 +62,10 @@ export default function DeviceDetailSidebar({
         <Popover>
           <Popover.Panel
             static
-            className="fixed z-20 top-0 right-0 h-full w-96 bg-white border border-gray-200 shadow-[-2px_0_15px_rgba(0,0,0,0.3)]"
+            className="fixed z-20 top-0 right-0 h-full w-96 bg-white border border-gray-200 shadow-[-2px_0_15px_rgba(0,0,0,0.3)] overflow-y-auto"
           >
-            <div className="mt-24 p-4">
-              <div className="flex flex-row justify-end items-start">
+            <div className="mt-6 p-4">
+              <div className="flex flex-row justify-end items-start mb-4">
                 <XMarkIcon
                   className="h-6 w-6 text-gray-800 cursor-pointer"
                   onClick={() => setSelectedProvider(null)}
@@ -44,14 +78,51 @@ export default function DeviceDetailSidebar({
                 </>
               )}
 
-              {!isPending && (
-                <div>
-                  <p className="text-gray-800 font-semibold">
-                    {selectedProvider?.client_id}
-                  </p>
-                  <p className="text-gray-600 text-sm">
-                    Created on {provider?.created_time}
-                  </p>
+              {!isPending && provider && (
+                <div className="flex flex-col gap-6">
+                  {/* Title (device name) */}
+                  <div>
+                    <p className="text-gray-800 font-semibold">
+                      {selectedProvider?.client_id}
+                    </p>
+                    <p className="text-gray-600 text-sm">
+                      Created on {provider?.created_time}
+                    </p>
+                  </div>
+
+                  {/* Tab bar */}
+                  <div></div>
+
+                  {/* Uptime graphic */}
+                  <div className="">
+                    <p className="text-sm text-gray-400 font-semibold mb-2">
+                      Uptime (past 24 hours)
+                    </p>
+                    <div className="w-full h-8 bg-primary" />
+                  </div>
+
+                  {/* Charts */}
+                  <div className="">
+                    <p className="text-sm text-gray-400 font-semibold mb-2">
+                      Past 90 days
+                    </p>
+                    <div className="flex flex-col gap-2 mt-4">
+                      {charts.map((chart) => (
+                        <>
+                          <div className="relative bg-gray-100 w-full rounded-md h-40 pt-6">
+                            <div className="z-10 absolute top-2 left-2 text-sm font-semibold text-gray-400">
+                              {chart.name}
+                            </div>
+                            <BarChart
+                              name={chart.name}
+                              unit={chart.unit}
+                              data={provider[chart.key]}
+                            />
+                          </div>
+                        </>
+                      ))}
+                    </div>
+                  </div>
                 </div>
               )}
             </div>
