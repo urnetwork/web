4265547f997b58dbd0a85b6f8b9a8172297e04de
Awais Hussain <aw@ishussa.in>
2024-01-26 15:08:32 -0500

    Remove mock data for API calls

--- app/src/app/_lib/api.ts
+++ app/src/app/_lib/api.ts
@@ -86,8 +86,8 @@ async function makePostRequest(endpoint: string, body: object) {
 }
 
 /**
- * Similar to a GET request, however this doesn't parse the response as json
- * and instead returns a Blob() object.
+ * Similar to a GET request, however this returns a Blob() object instead
+ * of parsing the response as json.
  */
 async function makeResourceRequest(endpoint: string) {
   const byJwt = getJwt();
@@ -145,7 +145,6 @@ export async function postStatsProviderLast90(body: {
 export async function postRemoveNetworkClient(body: {
   client_id: string;
 }): Promise<RemoveNetworkClientResult> {
-  return {};
   return makePostRequest("network/remove-client", body);
 }
 
@@ -159,15 +158,6 @@ export async function postDeviceSetProvide(body: {
 export async function postDeviceAdd(body: {
   code: string;
 }): Promise<DeviceAddResult> {
-  const codeType = body.code.startsWith("s") ? "share" : "adopt";
-  await new Promise((r) => setTimeout(r, 1000));
-  return {
-    code_type: codeType,
-    code: body.code,
-    network_name: "test.bringyour.network",
-    client_id: "test_client_id",
-  };
-
   return makePostRequest("/device/add", body);
 }
 
@@ -175,11 +165,6 @@ export async function postDeviceCreateShareCode(body: {
   client_id: string;
   device_name: string;
 }): Promise<DeviceCreateShareCodeResult> {
-  await new Promise((r) => setTimeout(r, 1000));
-  return {
-    share_code: "s19023hioj1n3jlk2n1l3kn912",
-  };
-
   return makePostRequest("/device/create-share-code", body);
 }
 
@@ -193,11 +178,6 @@ export async function getDeviceShareCodeQR(share_code: string) {
 export async function postDeviceShareStatus(body: {
   share_code: string;
 }): Promise<DeviceShareStatusResult> {
-  await new Promise((r) => setTimeout(r, 5000));
-  return {
-    pending: false,
-    associated_network_name: "test.bringyour.network",
-  };
   return makePostRequest("/device/share-status", body);
 }
 
@@ -205,81 +185,22 @@ export async function postDeviceConfirmShare(body: {
   share_code: string;
   confirm: boolean;
 }): Promise<DeviceConfirmShareResult> {
-  await new Promise((r) => setTimeout(r, 2000));
-  return {
-    complete: true,
-    associated_network_name: "test.bringyour.network",
-  };
   return makePostRequest("/device/confirm-share", body);
 }
 
 export async function postDeviceAdoptStatus(body: {
   share_code: string;
 }): Promise<DeviceAdoptStatusResult> {
-  await new Promise((r) => setTimeout(r, 1000));
-  return {
-    pending: true,
-    associated_network_name: "test.bringyour.network",
-  };
   return makePostRequest("/device/adopt-status", body);
 }
 
 export async function getDeviceAssociations(): Promise<DeviceAssociationsResult> {
-  await new Promise((r) => setTimeout(r, 1000));
-  return {
-    pending_adoption_devices: [
-      {
-        code: "4a6f373254ff802590a75ec030ac57e7",
-        device_name: "test_pending_adoption",
-        client_id: "c48b43c2-6629-4cb2-9bb1-882c21f5b2b2",
-      },
-    ],
-    incoming_shared_devices: [
-      {
-        pending: true,
-        code: "f62f82bd648e013366227af68d5ae512",
-        device_name: "test_incoming_shared_device",
-        client_id: "829ce7df-0522-47c7-84ad-e48117b3f16f",
-        network_name: "test.bringyour.network",
-      },
-      {
-        pending: false,
-        code: "asdj1213ae013366227af68d5ae512",
-        device_name: "test_incoming_shared_device_confirmed",
-        client_id: "0808e61f-84a8-4844-ac04-dc6a4847ad39",
-        network_name: "test.bringyour.network",
-      },
-    ],
-    outgoing_shared_devices: [
-      {
-        pending: true,
-        code: "7ad2747fc5ca767577493f923a217aeb",
-        device_name: "Awais second test device",
-        client_id: "018d46d0-1ec7-f740-9248-28ab9cae20cd",
-        network_name: "test.bringyour.network",
-      },
-      {
-        pending: false,
-        code: "7ad2747fc5ca767577493f923a217aeb",
-        device_name: "Awais second test device",
-        client_id: "018d46d0-1ec7-f740-9248-28ab9cae20cd",
-        network_name: "test2.bringyour.network",
-      },
-    ],
-  };
-
   return makeGetRequest("/device/associations");
 }
 
 export async function postDeviceRemoveAssociation(body: {
   code: string;
 }): Promise<DeviceRemoveAssociationResult> {
-  await new Promise((r) => setTimeout(r, 1000));
-  return {
-    client_id: "temp_client_id",
-    network_name: "test.bringyour.network",
-  };
-
   return makePostRequest("/device/remove-association", body);
 }
 
