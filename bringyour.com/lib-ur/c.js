4971e75c8c45bc3ae85ce12c4bdd34d413e3a51c
Brien Colwell <xcolwell@gmail.com>
2024-11-06 22:41:37 -0800

    deep links: update

--- bringyour.com/lib-ur/c.js
+++ bringyour.com/lib-ur/c.js
@@ -6,9 +6,22 @@ new function() {
     setTimeout(function(){
         self.initLinks()
 
-        // attempt to redirect to the deep link
-    	let deepLink = `ur://${window.location.search}`
-    	window.location.replace(deepLink)
+        // note that if universal deep links or android deep links are working correctly
+        // we will not be on the html page
+        // since this script is evaluating, it means the deep link did not work
+        // - on android chrome, deep links will not always work when tapping a link.
+        //   we will try to upgrade to the app using the custom scheme
+        // - Apple webkit pops variants of a dialog "Safari cannot open the page because the address is invalid."
+        //   if the custom scheme is not registered.
+        //   we do not try a custom scheme on Apple WebKit
+        if (!self.isAppleWebkit()) {
+            // attempt to redirect to the scheme deep link
+            let params = new URLSearchParams(window.location.search)
+            // any app can intercept the custom scheme
+            params.delete('auth_code')
+        	let deepLink = `ur://ur.io/c${params.toString()}`
+        	window.location.replace(deepLink)
+        }
     }, 0)
 
     self.initLinks = function() {
@@ -53,5 +66,13 @@ new function() {
 	self.isApple = function() {
 	    return /iPhone|iPad|iPod/i.test(navigator.userAgent)
 	}
+
+    self.isAppleWebkit = function() {
+        // see https://stackoverflow.com/questions/7944460/detect-safari-browser
+        return navigator.vendor && 0 <= navigator.vendor.indexOf('Apple')
+               // navigator.userAgent &&
+               // navigator.userAgent.indexOf('CriOS') == -1 &&
+               // navigator.userAgent.indexOf('FxiOS') == -1;
+    }
 }()
 
